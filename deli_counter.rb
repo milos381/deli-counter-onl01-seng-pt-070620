f9637d0b3438285bb24e6d24f9dedfac27ea6dae
Milos Popovic <milos_popovic@outlook.com>
2020-07-09 20:42:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,15 +5,28 @@ def line(array)
   else
     new_array = []
     array.each_with_index do |i, index|
-      #{index + 1}. #{i}
-      new_array.push("#{index + 1}. #{i}").join(" ")
+
+      new_array.push("#{index + 1}. #{i}")
 
     end
-    puts "The line is currently: " + new_array
+   puts "The line is currently: " + new_array.join(" ")
   end
 end
+
 def take_a_number(array, name)
-  array.each do |i|
-    puts "Welcome, #{name}. You are number #{i+1} in line"
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+  #if array.length > 0
+  #  array.push(name)
+  #  currentSpot = array.index(name)
+#  end
+#  puts "Welcome, #{name}. You are number #{currentSpot} in line."
+end
+def now_serving(array)
+  if array.length < 1
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
   end
 end
